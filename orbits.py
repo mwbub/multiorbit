a3e7d2a29a34756ec790b4c4417a169a9285ce00
Mathew Bub <mathew.bub@gmail.com>
2018-09-29 12:33:35 -0400

    Orbits.integrate_chunks now initializes orbits internally

--- orbits.py
+++ orbits.py
@@ -27,12 +27,12 @@ class Orbits:
                 3) Astropy (>v3.0) SkyCoord that includes velocities (this turns
                    on physical output even if ro and vo are not given).
                 4) [ra, dec, d, mu_ra, mu_dec, vlos] in [deg, deg, kpc, mas/yr,
-                   mas/yr, km/s] (all J2000.0; mu_ra = mu_ra * cos dec); can be 
+                   mas/yr, km/s] (all J2000.0; mu_ra = mu_ra * cos dec); can be
                    Quantities; ICRS frame.
                 5) [ra, dec, d, U, V, W] in [deg, deg, kpc, km/s, km/s, kms];
                    can be Quantities; ICRS frame.
                 6) [l, b, d, mu_l, mu_b, vlos] in [deg, deg, kpc, mas/yr,
-                   mas/yr, km/s) (all J2000.0; mu_l = mu_l * cos b); can be 
+                   mas/yr, km/s) (all J2000.0; mu_l = mu_l * cos b); can be
                    Quantities.
                 7) [l, b, d, U, V, W] in [deg,deg,kpc,km/s,km/s,kms]; can be
                    Quantities.
@@ -43,9 +43,10 @@ class Orbits:
             uvw (bool, optional): If True, velocities are UVW.
             lb (bool; optional): If True, input is 6) or 7) above (note that
                 this turns on physical output even if ro and vo are not given).
-            ro (float): Distance from the vantage point to the GC (kpc; can be
+            ro (float; optional): Distance from the vantage point to the GC
+                (kpc; can be Quantity).
+            vo (float; optional): Circular velocity at ro (km/s; can be
                 Quantity).
-            vo (float): Circular velocity at ro (km/s; can be Quantity).
             zo (float; optional): Offset toward teh NGP of the Sun wrt the plane
                 (kpc; can be Quantity; default = 25 pc).
             solarmotion (optional): 'hogg', or 'dehnen', or 'schoenrich', or
@@ -114,41 +115,81 @@ class Orbits:
         self.orbits = list(parallel_map(integrate, self.orbits,
                                         numcores=numcores))
 
-    def integrate_chunks(self, t, pot, filename, method='symplec4_c', dt=None,
+    @classmethod
+    def integrate_chunks(cls, t, pot, filename, vxvv=None, radec=False,
+                         uvw=False, lb=False, ro=None, vo=None, zo=None,
+                         solarmotion=None, method='symplec4_c', dt=None,
                          numcores=1, chunk_size=1000000, save_all=False):
-        """Integrate these Orbits in chunks and save the results in a file.
+        """Create and integrate Orbits in a series of chunks.
 
-        When using multiprocessing, this method does not update the values of
-        the individual orbit instances, but rather saves the integrated values
-        in a FITS file. Note that the file name will be modified by appending
-        a '_0', '_1', '_2', etc. in order to number each time step.
+        This method is designed to integrate a large number of orbits in a
+        series of chunks. Due to the memory requirements of each Orbit instance,
+        this method creates and integrates all orbits internally, and saves
+        the results in a FITS file. As such, it accepts the parameters of both
+        the Orbits.integrate method and the Orbits initialization keywords.
 
         Args:
             t: List of times at which to output, including 0; can be Quantity.
             pot: Potential instance of list of instances.
             filename: File in which to save the results of the integration.
-                The file will be saved in the FITS format.
-            method (optional): 'odeint' for scipy's odeint;
+                The file will be saved in the FITS format. Note that the file
+                name will be modified by appending a '_0', '_1', '_2', etc.
+                in order to number each time step.
+            vxvv: List of initial conditions. Elements can be either:
+                1) Orbit instance.
+                2) In galactocentric cylindrical coordinates [R, vR, vT, (z, vz,
+                   phi)]; can be Quantities.
+                3) Astropy (>v3.0) SkyCoord that includes velocities (this turns
+                   on physical output even if ro and vo are not given).
+                4) [ra, dec, d, mu_ra, mu_dec, vlos] in [deg, deg, kpc, mas/yr,
+                   mas/yr, km/s] (all J2000.0; mu_ra = mu_ra * cos dec); can be
+                   Quantities; ICRS frame.
+                5) [ra, dec, d, U, V, W] in [deg, deg, kpc, km/s, km/s, kms];
+                   can be Quantities; ICRS frame.
+                6) [l, b, d, mu_l, mu_b, vlos] in [deg, deg, kpc, mas/yr,
+                   mas/yr, km/s) (all J2000.0; mu_l = mu_l * cos b); can be
+                   Quantities.
+                7) [l, b, d, U, V, W] in [deg,deg,kpc,km/s,km/s,kms]; can be
+                   Quantities.
+                8) Unspecified: assumed to be the Sun (equivalent to vxvv=[0, 0,
+                   0, 0, 0, 0] and radec=True).
+                All elements must be of the same form.
+
+        Optional Args:
+            radec: If True, vxvv is 4) or 5) above.
+            uvw: If True, velocities are UVW.
+            lb: If True, vxvv is 6) or 7) above (note that this turns on
+                physical output even if ro and vo are not given).
+            ro: Distance from the vantage point to the GC (kpc; can be
+                Quantity).
+            vo: Circular velocity at ro (km/s; can be Quantity).
+            zo: Offset toward teh NGP of the Sun wrt the plane (kpc; can be
+                Quantity; default = 25 pc).
+            solarmotion: 'hogg', or 'dehnen', or 'schoenrich', or value in
+                [-U, V, W]; can be Quantity.
+            method: 'odeint' for scipy's odeint;
                 'leapfrog' for a simple leapfrog implementation;
                 'leapfrog_c' for a simple leapfrog implementation in C;
                 'symplec6_c' for a 6th order symplectic integrator in C;
                 'rk4_c' for a 4th-order Runge-Kutta integrator in C;
                 'rk6_c' for a 6-th order Runge-Kutta integrator in C;
                 'dopr54_c' for a Dormand-Prince integrator in C.
-            dt (optional): If set, force the integrator to use this basic
-                step size; must be an integer divisor of output step size (only
-                works for C integrators that use a fixed step size); can be
-                Quantity.
-            numcores (optional): Number of cores to use for multiprocessing.
-            chunk_size (optional): Number of orbits to integrate per chunk;
+            dt: If set, force the integrator to use this basic step size; must
+                be an integer divisor of output step size (only works for C
+                integrators that use a fixed step size); can be Quantity.
+            numcores: Number of cores to use for multiprocessing.
+            chunk_size: Number of orbits to integrate per chunk;
                 default = 1000000.
-            save_all (optional): If True, save all time steps. Otherwise, save
-                only the final time step. Default = False
+            save_all: If True, save all time steps. Otherwise, save only the
+                final time step. Default = False
 
         Returns:
             None
 
         """
+        if vxvv is None:
+            vxvv = []
+
         # Remove the .fits file extension if it was provided
         if filename.lower()[-5:] == '.fits':
             filename = filename[:-5]
@@ -170,9 +211,10 @@ class Orbits:
             hdu.writeto(file, overwrite=True)
 
         # Iterate over chunks of orbits of size chunk_size
-        for i in range(0, len(self.orbits), chunk_size):
-            # Integrate the chunk
-            chunk = Orbits(vxvv=self.orbits[i:i+chunk_size])
+        for i in range(0, len(vxvv), chunk_size):
+            # Generate and integrate the Orbits chunk
+            chunk = cls(vxvv=vxvv[i:i+chunk_size], radec=radec, uvw=uvw, lb=lb,
+                        ro=ro, vo=vo, zo=zo, solarmotion=solarmotion)
             chunk.integrate(t, pot, method=method, dt=dt, numcores=numcores)
 
             # Update the file for each time step
