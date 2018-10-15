aaee69213cf83325d9c22507b2258d70cbaba7cd
Mathew Bub <mathew.bub@gmail.com>
2018-10-14 20:40:41 -0400

    Update setup.py url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='multiorbit',
     version='0.1.dev',
     packages=['multiorbit'],
-    url='https://github.com/mwbub/parallel-orbit-integration',
+    url='https://github.com/mwbub/multiorbit',
     author='Mathew Bub',
     author_email='mathew.bub@gmail.com',
     description='Package for running large orbit integration suites in galpy.',
